fe533fb47a48dac51f467b9e2c2759970d3cbda6
Your Name <you@example.com>
2025-07-12 22:24:11 +0200

    modified:   .gitignore 	new file:   kurator/.env 	modified:   kurator/eslint.config.js 	modified:   kurator/index.html 	modified:   kurator/package-lock.json 	modified:   kurator/package.json 	modified:   kurator/postcss.config.cjs 	modified:   kurator/server.js 	deleted:    kurator/src/App.css 	modified:   kurator/src/App.jsx 	modified:   kurator/src/index.css 	modified:   kurator/src/main.jsx 	modified:   kurator/tailwind.config.cjs 	modified:   kurator/vite.config.js

--- kurator/tailwind.config.cjs
+++ kurator/tailwind.config.cjs
@@ -1,10 +1,8 @@
+/** @type {import('@tailwindcss/postcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
