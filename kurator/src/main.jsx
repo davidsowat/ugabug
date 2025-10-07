461b893b0cf601facce416f37d3060c1e10a405b
Your Name <you@example.com>
2025-10-07 23:49:06 +0200

    modified:   kurator/src/main.jsx

--- kurator/src/main.jsx
+++ kurator/src/main.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Importera BrowserRouter
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import LoginPage from "./LoginPage.jsx";
+import AuthCallback from "./AuthCallback.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";             // behåll dina css
+import "./PlaylistSelector.css";
+import "./CriteriaSelection.css";
+import "./ResultPage.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/app" element={<App />} />
+        <Route path="/callback" element={<AuthCallback />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
